1404b90b060da9f888016c29304625dec54bb39b
dosheena93 <141981375+dosheena93@users.noreply.github.com>
2023-09-25 21:46:28 -0700

    Add files via upload
    Built a temperature converter. This page prompts the user to enter a number, and it will convert it from Fahrenheit to Celsius and let the user know if the temperature is hot or cold.

--- index.js
+++ index.js
@@ -5,8 +5,10 @@
  * @param {string} fahren temperature in degrees F
  * @returns {number} the number of degrees C
  */
-function convertToCelsius(fahren) {}
-
+function convertToCelsius(fahrenheit) {
+const celsius = (fahrenheit - 32) * 5/9;
+  return celsius;
+}
 /**
  * Takes both numbers (F, C) and display a message with
  * both numbers and how that temp makes you feel using
@@ -22,14 +24,43 @@ function convertToCelsius(fahren) {}
  * @param {number} fahren
  * @param {number} celc
  */
-function createMessage(fahren, celc) {}
+function createMessage(fahrenheit, celsius) {
+  let message;
+  if (fahrenheit < 32) {
+    message = `${fahrenheit} degrees Fahrenheit (${celsius.toFixed(2)} degrees Celsius) feels very cold.`;
+  } else if (fahrenheit < 64) {
+    message = `${fahrenheit} degrees Fahrenheit (${celsius.toFixed(2)} degrees Celsius) feels cold.`;
+  } else if (fahrenheit < 86) {
+    message = `${fahrenheit} degrees Fahrenheit (${celsius.toFixed(2)} degrees Celsius) feels warm.`;
+  } else {
+    message = `${fahrenheit} degrees Fahrenheit (${celsius.toFixed(2)} degrees Celsius) feels hot.`;
+  }
+  return message;
+}
+
+
+function convertAndDisplay() {
+  const fahrenheitInput = document.getElementById("fahrenheitInput");
+  const resultElement = document.getElementById("result");
+
+  const fahrenheit = parseFloat(fahrenheitInput.value);
+  if (!isNaN(fahrenheit)) {
+    const celsius = convertToCelsius(fahrenheit);
+    const message = createMessage(fahrenheit, celsius);
+    resultElement.textContent = message;
+  } else {
+    resultElement.textContent = "Please enter a valid number.";
+  }
+}
 
 /**
  * Takes a number and returns a random integer from 0 to the limit
  * @param {number} limit
  * @returns {number} a number between 0 and the int passed in
  */
-function rand(limit) {}
+function rand(limit) {
+  return Math.floor(Math.random() * (limit + 1));
+}
 
 // -------------------- DONT NOT CHANGE THE CODE BELOW ---------------------- //
 
